5163bffa127f26ef1cdedf54e5c38addca4af52f
Gomding <backchida@gmail.com>
2020-07-20 22:38:41 +0900

    통계 메인페이지 작업

---
+++ src/main/java/com/community/hululuuuu/controller/StatsController.java
@@ -0,0 +1,19 @@
+package com.community.hululuuuu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/stats")
+public class StatsController {
+
+    @GetMapping("/statsMain")
+    public ModelAndView main() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("stats/statsMain");
+        return mav;
+    }
+
+}
